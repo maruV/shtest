88f85a7de3d7db940b7a61a1d467d5d75bed45ed
Vishal Maru <maruvishal35@gmail.com>
2021-05-08 03:55:56 +0530

    added some icons, improved home and other component scss

--- gc-client/src/components/Navbar.jsx
+++ gc-client/src/components/Navbar.jsx
@@ -1,32 +1,38 @@
 import React, {Component} from 'react';
 import {Link, NavLink} from "react-router-dom";
+import mainlogo from "../assets/mainlogo.png";
 
 class Navbar extends Component{
     render(){
         return (
-            <div className="mainNav">
-                <div className="navBarContainer">
+            <div className="navBarContainer">
+                
+                <div className="leftnavContainer">
                     <div className="HomeLinkContainer">
-                        <Link exact to="/" ><p> Home This will be an img tag</p></Link>
+                        <NavLink exact to="/" >
+                            <img src={mainlogo} />
+                        </NavLink>
                     </div>
+                </div>
+
+                <div className="rightnavContainer">
 
                     <div className="bbLinkContainer">
-                        <NavLink to="/bulkbuy" activeStyle={{color:"#f6a500"}} ><p> Bulk Buy Gift Cards </p></NavLink>
+                        <NavLink to="/BulkBuy" activeStyle={{color:"#f6a500"}} ><p> Bulk Buy Gift Cards </p></NavLink>
                     </div>
 
                     <div className="rLinkContainer">
-                        <NavLink to="/register" activeStyle={{color:"#f6a500"}} ><p> Sell Gift Cards </p></NavLink>
+                        <NavLink to="/NotImplemented" activeStyle={{color:"#f6a500"}} ><p> Sell Gift Cards </p></NavLink>
                     </div>
-
-                    <div className="LinkContainer">
-                        <NavLink to="/notImplemented" activeStyle={{color:"#f6a500"}} ><p> Sell Gift Cards </p></NavLink>
+                    <div className="niLinkContainer">
+                        <NavLink to="/Register" activeStyle={{color:"#f6a500"}} ><p> Account </p></NavLink>
+                    </div>
+                    <div className="niLinkContainer">
+                        <NavLink to="/NotImplemented" activeStyle={{color:"#f6a500"}} ><p> # </p></NavLink>
                     </div>
                 </div>
-
-                <div className="navBarContainer static-push">
-                </div>
-
             </div>
+
         )
     }
 }
--- gc-client/src/pages/Home.jsx
+++ gc-client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 
+import bulkicon from "../assets/bulkicon.png";
 
 class Home extends Component {
     render(){
         return (
             <div className="homeBlock">
                 <div className="topContainer">
+
                     <div className="slantContentContainer">
                         <div className="orangeBox"></div>
                         <div className="videoContainer"></div>
@@ -14,36 +16,43 @@ class Home extends Component {
                     <div className="introTextContainer">
                         <div className="leftContainer">
                             <div className="titleContainer">
-                                <p>icon</p>
-                                <p>Buy Gift Cards</p>
+                                <div className="imagewrapper">
+                                    <img src={bulkicon} />
+                                </div>
+                                
+                                <div className="t1"><p> Bulk Buy Gift Cards </p></div>
                             </div>
                             <div className="homeTextContainer">
                                 <div className="headerText">
-                                    <p><b>Gift Card Solutions</b></p>
-                                    <p>for your Business</p>
+                                    <p id="h1"><b>Gift Card Solutions</b></p>
+                                    <p id="h2">for your Business</p>
                                 </div>
                                 <div className="infoText">
-                                    <p></p>
+                                    <p> Cras mattis consectetur purus sit amet </p>
+                                    <p> fermentum. Etiam porta sem malesuada </p>
+                                    <p> magna mollis euismod. </p>
                                 </div>
                             </div>
-                            <div>
-                                <button>BUY GIFT CARDS</button>
-                                <button>REGISTER TODAY</button>
+                            <div className="buttonsContainer">
+                                <button id="b1">BUY GIFT CARDS</button>
+                                <button id="b2">REGISTER TODAY</button>
                             </div>
                         </div>
                         <div className="rightContainer">
                             <p>Right Orange icon</p>
                         </div>
-                        <div className="bottomTextContainer">
-                            <div>
-                                <p><b>Peace of Mind</b> with every bulk purchase...</p>
-                            </div>
-                            <div>
-                                <div><p>Huge Discounts</p></div>
-                                <div><p>Safe and Secure</p></div>
-                                <div><p>Instant Delivery</p></div>
-                                <div><p>B2B Specialists</p></div>
-                            </div>
+                        
+                    </div>
+
+                    <div className="bottomTextContainer">
+                        <div className="catchphraseContainer">
+                            <p><b>Peace of Mind</b> with every bulk purchase...</p>
+                        </div>
+                        <div className="featuresContainer">
+                            <div><p>Huge Discounts</p></div>
+                            <div><p>Safe and Secure</p></div>
+                            <div><p>Instant Delivery</p></div>
+                            <div><p>B2B Specialists</p></div>
                         </div>
                     </div>
                 </div>
